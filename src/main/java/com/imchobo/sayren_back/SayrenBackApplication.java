536c7c57e4cb286221bbc2d74d69de43004e111d
sr0116 <kysy0116@gmail.com>
2025-09-08 17:42:28 +0900

    첫 메인 브랜치 생성

---
+++ src/main/java/com/imchobo/sayren_back/SayrenBackApplication.java
@@ -0,0 +1,13 @@
+package com.imchobo.sayren_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayrenBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SayrenBackApplication.class, args);
+  }
+
+}
