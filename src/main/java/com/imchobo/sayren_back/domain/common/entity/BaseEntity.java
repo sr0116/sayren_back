d61a18a4a97eecc09e5909ae3e7ff0ccb1a40d23
songseongjun <thdtjdwns0902@naver.com>
2025-09-10 17:54:13 +0900

    배송지설정및배송아이템값 데이터베이스 저장완료

---
+++ src/main/java/com/imchobo/sayren_back/domain/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.imchobo.sayren_back.domain.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  // DB 컬럼명이 regdate 라서 매핑 지정
+  @CreatedDate
+  @Column(name = "regdate", updatable = false)
+  private LocalDateTime regDate;
+
+  // DB 컬럼명이 moddate 라서 매핑 지정
+  @LastModifiedDate
+  @Column(name = "moddate")
+  private LocalDateTime modDate;
+}
---
+++ src/main/java/com/imchobo/sayren_back/domain/common/entity/TimeRangeEntity.java
@@ -0,0 +1,40 @@
+package com.imchobo.sayren_back.domain.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeRangeEntity {
+
+  @CreatedDate
+  @Column(updatable = false)
+  private LocalDateTime regdate;  //생성일
+
+  @Column
+  private LocalDateTime voiddate;  //만료일
+
+  // 즉시 만료
+  public void expire() {
+    this.voiddate = LocalDateTime.now();
+  }
+
+
+  // 만료시간 지정
+  public void expireAt(LocalDateTime expireTime) {
+    this.voiddate = expireTime;
+  }
+
+  // 만료되었는지 검증
+  public boolean isExpired() {
+    return voiddate != null && voiddate.isBefore(LocalDateTime.now());
+  }
+}
