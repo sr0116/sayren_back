dc9d5d5b53001a4cbdebbfeb5e60f244d64ad327
jiyu196 <jiyu16413@gmail.com>
2025-09-15 18:33:18 +0900

    초기화

--- src/main/java/com/imchobo/sayren_back/domain/product/repository/ProductTagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.imchobo.sayren_back.domain.product.repository;
-
-import com.imchobo.sayren_back.domain.product.entity.ProductTag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductTagRepository extends JpaRepository<ProductTag, Long> {
-}
--- src/main/java/com/imchobo/sayren_back/repository/AttachCrawlRepository.java
+++
@@ -1,8 +0,0 @@
-package com.imchobo.sayren_back.repository;
-
-import com.imchobo.sayren_back.entity.AttachCrawl;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-  public interface AttachCrawlRepository extends JpaRepository<AttachCrawl, Long> {
-
-  }
