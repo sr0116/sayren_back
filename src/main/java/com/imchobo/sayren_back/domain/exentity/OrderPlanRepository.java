0dd1e412a431d23b87312badffa55d96d7ae182e
jiyu196 <jiyu16413@gmail.com>
2025-09-16 15:47:51 +0900

    병합

--- src/main/java/com/imchobo/sayren_back/domain/exentity/OrderPlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imchobo.sayren_back.domain.exentity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderPlanRepository extends JpaRepository<OrderPlan, Long> {
-  /// //////// 임시 /////////////////
-
-
-}
