c6809f1119023bd1fdffc68c20a8b962aaa03bd2
jiyu196 <jiyu16413@gmail.com>
2025-09-16 18:21:06 +0900

    병합중

--- src/main/java/com/imchobo/sayren_back/domain/attach/repository/AttachRepository.java
+++
@@ -1,7 +0,0 @@
-package com.imchobo.sayren_back.domain.attach.repository;
-
-import com.imchobo.sayren_back.domain.attach.entity.Attach;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttachRepository extends JpaRepository<Attach, Long> {
-}
--- src/main/java/com/imchobo/sayren_back/domain/board/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.imchobo.sayren_back.domain.board.repository;
-
-import com.imchobo.sayren_back.domain.board.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
