44ffb5dbe33ba5b2c3c3d4ddd291b7937803b5eb
sr0116 <kysy0116@gmail.com>
2025-09-22 02:27:57 +0900

    구독 결제 준비시 subscribe history 생성

--- src/main/java/com/imchobo/sayren_back/domain/common/en/ReasonCode.java
+++ src/main/java/com/imchobo/sayren_back/domain/common/en/ReasonCode.java
@@ -2,11 +2,16 @@ package com.imchobo.sayren_back.domain.common.en;
 
 public enum ReasonCode {
   NONE, // 기본상태(환불없음)
-  USER_REQUEST,     // 회원 요청 환불
+  USER_REQUEST,     // 회원 요청
+  AUTO_REFUND, // 24시간 이내 자동 환불
   PRODUCT_DEFECT,   // 상품 불량
+  DELIVERY_ISSUE, // 배송 문제(지연, 오배송)
+  OUT_OF_STOCK, // 재고 부족
   SERVICE_ERROR,    // 서비스 오류
-  PAYMENT_FAILURE,  // 결제 실패 후 환불
+  SYSTEM_ERROR, // 시스템 장애
+  PAYMENT_FAILURE,  // 결제 실패
   CANCEL_REJECTED, // 취소 요청 거절
-  CONTRACT_CANCEL,  // 계약 해지
+  CONTRACT_CANCEL,  // 계약 해지 (중도 해지)
+  EXPIRED, // 계약 만료(환급)
   OTHER             // 기타
 }
