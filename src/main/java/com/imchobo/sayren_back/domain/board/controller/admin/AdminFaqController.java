bf9697663f697fc921e634f3e085d5a970488ca8
jiyu196 <jiyu16413@gmail.com>
2025-10-10 01:07:39 +0900

    board 관리자/유저 컨트롤러 분류 및 관리자 페이지에서 상품 등록할 수 있도록 컨트롤러, 서비스, 서비스 구현체 코드 추가

--- src/main/java/com/imchobo/sayren_back/domain/board/controller/admin/AdminFaqController.java
+++ src/main/java/com/imchobo/sayren_back/domain/board/controller/admin/AdminFaqController.java
@@ -1,6 +1,7 @@
-package com.imchobo.sayren_back.domain.board.controller;
+package com.imchobo.sayren_back.domain.board.controller.admin;
 
 import com.imchobo.sayren_back.domain.board.entity.Board;
+import com.imchobo.sayren_back.domain.board.service.admin.AdminFaqService;
 import com.imchobo.sayren_back.domain.common.dto.PageRequestDTO;
 import com.imchobo.sayren_back.domain.common.dto.PageResponseDTO;
 import com.imchobo.sayren_back.domain.board.dto.faq.FaqCreateRequestDTO;
@@ -17,41 +18,41 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin/faqs")
 @RequiredArgsConstructor
-public class FaqController {
+public class AdminFaqController {
 
-  private final FaqService faqService;
+  private final AdminFaqService adminFaqService;
 
   @PostMapping
   public ResponseEntity<Long> register(@RequestBody FaqCreateRequestDTO dto) {
-    return ResponseEntity.ok(faqService.register(dto));
+    return ResponseEntity.ok(adminFaqService.register(dto));
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<Void> modify(@PathVariable Long id, @RequestBody FaqModifyRequestDTO dto) {
-    faqService.modify(id, dto);
+    adminFaqService.modify(id, dto);
     return ResponseEntity.noContent().build();
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> delete(@PathVariable Long id) {
-    faqService.delete(id);
+    adminFaqService.delete(id);
     return ResponseEntity.noContent().build();
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<FaqDetailsResponseDTO> read(@PathVariable Long id) {
-    return ResponseEntity.ok(faqService.read(id));
+    return ResponseEntity.ok(adminFaqService.read(id));
   }
 
   // 전체목록 조회(페이징 전)
   @GetMapping
-  public ResponseEntity<List<FaqListResponseDTO>> list() {
-    return ResponseEntity.ok(faqService.list());
+  public ResponseEntity<List<FaqListResponseDTO>> allFaqs() {
+    return ResponseEntity.ok(adminFaqService.list());
   }
 
   // 페이징 처리 전체목록 조회
   @GetMapping("/list")
   public ResponseEntity<PageResponseDTO<FaqListResponseDTO, Board>> list(PageRequestDTO requestDTO) {
-    return ResponseEntity.ok(faqService.getList(requestDTO));
+    return ResponseEntity.ok(adminFaqService.getList(requestDTO));
   }
 }
--- src/main/java/com/imchobo/sayren_back/domain/board/controller/admin/AdminNoticeController.java
+++ src/main/java/com/imchobo/sayren_back/domain/board/controller/admin/AdminNoticeController.java
@@ -1,6 +1,7 @@
-package com.imchobo.sayren_back.domain.board.controller;
+package com.imchobo.sayren_back.domain.board.controller.admin;
 
 import com.imchobo.sayren_back.domain.board.entity.Board;
+import com.imchobo.sayren_back.domain.board.service.admin.AdminNoticeService;
 import com.imchobo.sayren_back.domain.common.dto.PageRequestDTO;
 import com.imchobo.sayren_back.domain.common.dto.PageResponseDTO;
 import com.imchobo.sayren_back.domain.board.dto.notice.NoticeCreateRequestDTO;
@@ -17,40 +18,40 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin/notices")
 @RequiredArgsConstructor
-public class NoticeController {
+public class AdminNoticeController {
 
-  private final NoticeService noticeService;
+  private final AdminNoticeService adminNoticeService;
 
   @PostMapping
   public ResponseEntity<Long> register(@RequestBody NoticeCreateRequestDTO dto) {
-    return ResponseEntity.ok(noticeService.register(dto));
+    return ResponseEntity.ok(adminNoticeService.register(dto));
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<Void> modify(@PathVariable Long id, @RequestBody NoticeModifyRequestDTO dto) {
-    noticeService.modify(id, dto);
+    adminNoticeService.modify(id, dto);
     return ResponseEntity.noContent().build();
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> delete(@PathVariable Long id) {
-    noticeService.delete(id);
+    adminNoticeService.delete(id);
     return ResponseEntity.noContent().build();
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<NoticeDetailsResponseDTO> read(@PathVariable Long id) {
-    return ResponseEntity.ok(noticeService.read(id));
+    return ResponseEntity.ok(adminNoticeService.read(id));
   }
 
   @GetMapping
   public ResponseEntity<List<NoticeListResponseDTO>> list() {
-    return ResponseEntity.ok(noticeService.list());
+    return ResponseEntity.ok(adminNoticeService.list());
   }
 
   // 페이징
   @GetMapping("/list")
   public ResponseEntity<PageResponseDTO<NoticeListResponseDTO, Board>> getList(PageRequestDTO requestDTO) {
-    return ResponseEntity.ok(noticeService.getList(requestDTO));
+    return ResponseEntity.ok(adminNoticeService.getList(requestDTO));
   }
 }
